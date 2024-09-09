bcaac02d84835b8f0e72afada6d838668569bb95
Timo_Burnham <timoburnham@gmail.com>
2024-09-08 20:51:05 -0500

    added turning functionality

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, PLAYER_TURN_SPEED
+from player import Player
 
 def main():
 	pygame.init()
@@ -13,15 +13,18 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, 30)
 	running = True	
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				running = False
 		screen.fill(BLACK)
+		player.draw(screen)
+		player.update(dt)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000.0
-
 if __name__ == "__main__":
     main()
 
